91b3136e74ee099a987343a6cc65d32e10c3b89d
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2023-10-22 22:55:49 +0200

    🔒 Implement mostly working login with Supabase

---
+++ src/lib/components/custom/NavBar.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { LogOut } from 'lucide-svelte';
+    import { Button, buttonVariants } from "$lib/components/ui/button";
+    import toast from 'svelte-french-toast';
+    import { goto, invalidateAll } from '$app/navigation';
+    import { page } from '$app/stores';
+    import type { SupabaseClient } from '@supabase/supabase-js';
+    import type { Database } from '$lib/supabase-types';
+    export const ssr = false;
+    export let supabase: SupabaseClient<Database, "public", {
+        Tables: {};
+        Views: {};
+        Functions: {};
+        Enums: {};
+        CompositeTypes: {};
+    }>
+    async function handleGotoDashboard() {
+        let session = await supabase.auth.getSession();
+        if($page.data.session === null || session.data.session === null) {
+            goto("/login")
+        } else {
+            goto("/")
+        }
+    }
+
+    async function handleSignOut() {
+        const logOutResponse = await supabase.auth.signOut();
+        
+        if(logOutResponse.error === null) {
+            toast.success("Erfolgreich ausgeloggt");
+            goto("/login");
+        } else {
+            toast.error(`Fehler während des ausloggens: ${logOutResponse.error.message}`);
+        }
+    }
+</script>
+
+<nav class="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30">
+    <div class="max-w-5xl mx-auto px-3 md:px-0">
+        <div class="flex items-center justify-between h-16">
+            <p class="text-[13px] md:text-2xl text-gray-900 font-semibold">Marc's Klavier-Planer</p>
+            <div class="flex space-x-5 text-gray-900">
+                <Button href="/" data-sveltekit-reload class="sm:{buttonVariants({ size: "sm" })}">
+                    Dashboard
+                </Button>
+                <Button class="sm:{buttonVariants({ size: "sm" })} grid grid-rows-1 grid-flow-col gap-1 content-center" on:click={async () => await handleSignOut()}>
+                    <LogOut />
+                    <p>Ausloggen</p>
+                </Button>
+            </div>
+        </div>
+    </div>
+</nav>
