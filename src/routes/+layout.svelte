cb2b48b1153b5f03ff1ed1bca788a999b770a45e
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2023-10-24 17:08:33 +0200

    🌸 Set up ESLint and Prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,28 @@
 <script>
-    import "../app.postcss";
-    import { Toaster } from "svelte-french-toast";
-    import NavBar from "$lib/components/custom/NavBar.svelte";
-    import { invalidate } from '$app/navigation'
-    import { onMount } from 'svelte'
+	import '../app.postcss';
+	import { Toaster } from 'svelte-french-toast';
+	import NavBar from '$lib/components/custom/NavBar.svelte';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-  export let data
+	export let data;
 
-  let { supabase, session } = data
-  $: ({ supabase, session } = data)
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
 
-  onMount(() => {
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, _session) => {
-      if (_session?.expires_at !== session?.expires_at) {
-        invalidate('supabase:auth')
-      }
-    })
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
 
-    return () => subscription.unsubscribe()
-  });
+		return () => subscription.unsubscribe();
+	});
 </script>
 
-<Toaster/>
-<NavBar supabase={supabase}/>
+<Toaster />
+<NavBar {supabase} />
 <slot />
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,32 +1,32 @@
-import type { Actions } from "./$types";
+import type { Actions } from './$types';
 export const actions = {
-    register: async ({ request, url, locals: { supabase } }) => {
-        const formData = await request.formData();
-        const email = formData.get("user_email") as string
-        const password = formData.get("user_password") as string
-        const { error } = await supabase.auth.signUp({
-            email,
-            password,
-            options: {
-              emailRedirectTo: `${url.origin}/auth/callback`,
-            },
-        });
-        return {
-            action: "registriert",
-            error: error?.message === undefined ? null : error?.message
-        }
-    },
-    login: async ({ request, url, locals: { supabase } }) => {
-        const formData = await request.formData();
-        const email = formData.get("user_email") as string
-        const password = formData.get("user_password") as string
-        const { error } = await supabase.auth.signInWithPassword({
-            email,
-            password,
-        });
-        return {
-            action: "eingeloggt",
-            error: error?.message === undefined ? null : error?.message
-        }
-    }
+	register: async ({ request, url, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const email = formData.get('user_email') as string;
+		const password = formData.get('user_password') as string;
+		const { error } = await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${url.origin}/auth/callback`
+			}
+		});
+		return {
+			action: 'registriert',
+			error: error?.message === undefined ? null : error?.message
+		};
+	},
+	login: async ({ request, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const email = formData.get('user_email') as string;
+		const password = formData.get('user_password') as string;
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+		return {
+			action: 'eingeloggt',
+			error: error?.message === undefined ? null : error?.message
+		};
+	}
 } satisfies Actions;
