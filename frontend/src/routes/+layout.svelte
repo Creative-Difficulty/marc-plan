7ef7ef2435d603542253a764749d53a4ebf63f6d
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2023-12-12 22:03:23 +0100

    🛠️ Restructure project files

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { invalidateAll } from "$app/navigation";
+	import { pb } from "$lib/pocketbase";
+    import "../app.pcss";
+    import { Toaster } from 'svelte-french-toast';
+
+    pb.authStore.onChange(async () => {
+        if(pb.authStore.isValid === false) {
+            await invalidateAll();
+        }
+    })
+</script>
+<Toaster />
+<slot></slot>
